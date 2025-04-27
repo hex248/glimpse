1e65b64ed21aaf7811dd302f2068c2f5825b7edb
Oliver Bryan <65399431+hex248@users.noreply.github.com>
2025-04-27 10:34:45 +0100

    moved authOptions to lib file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,38 +1,5 @@
-import { prisma } from "@/lib/db";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth, { type AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { type Adapter } from "next-auth/adapters";
-
-export const authOptions: AuthOptions = {
-    adapter: PrismaAdapter(prisma) as Adapter,
-
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
-
-    session: {
-        strategy: "database",
-    },
-
-    callbacks: {
-        async session({ session, user }) {
-            if (session.user) {
-                session.user.id = user.id;
-                session.user.username = user.username;
-                session.user.color = user.color;
-            }
-            return session;
-        },
-    },
-
-    debug: process.env.NODE_ENV === "development",
-
-    secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ src/lib/authOptions.ts
@@ -0,0 +1,35 @@
+import { type AuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/lib/db";
+import { type Adapter } from "next-auth/adapters";
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma) as Adapter,
+
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+
+    session: {
+        strategy: "database",
+    },
+
+    callbacks: {
+        async session({ session, user }) {
+            if (session.user) {
+                session.user.id = user.id;
+                session.user.username = user.username;
+                session.user.color = user.color;
+            }
+            return session;
+        },
+    },
+
+    debug: process.env.NODE_ENV === "development",
+
+    secret: process.env.NEXTAUTH_SECRET,
+};
