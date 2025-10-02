27b9d76dba95ed9aa9c8f5f5c98c9b1c19a21b9e
Oliver Bryan <04oliverbryan@gmail.com>
2025-10-02 21:29:25 +0100

    call to action to search for users on homepage

--- src/components/photo-feed.tsx
+++ src/components/photo-feed.tsx
@@ -2,6 +2,9 @@
 
 import { type Prisma } from "@prisma/client";
 import FeedPhoto from "@/components/feed-photo";
+import Link from "next/link";
+import { APP_PATHS } from "@/lib/APP_PATHS";
+import { Search } from "lucide-react";
 
 type PhotoWithUser = Prisma.PhotoGetPayload<{
     include: { user: true };
@@ -13,17 +16,27 @@ export interface PhotoFeedProps {
 
 export default function PhotoFeed({ initialPhotos }: PhotoFeedProps) {
     return (
-        <div className="flex flex-row flex-wrap gap-4 justify-center">
+        <div className="flex flex-row flex-wrap justify-center">
             {initialPhotos.length === 0 && (
-                <p className="text-muted-foreground">
-                    No photos yet. Be the first to share!
-                </p>
+                <div className="flex flex-col items-center gap-4 text-lg">
+                    <p className="text-muted-foreground text-center">
+                        Send your friends a request to see their posts
+                    </p>
+                    <Link
+                        href={APP_PATHS.SEARCH.href}
+                        className="text-primary text-xl"
+                    >
+                        <span className="flex items-center gap-2 no-select">
+                            Search users
+                            <Search size={20} className="size-8" />
+                        </span>
+                    </Link>
+                </div>
             )}
 
             {initialPhotos.map((photo) => (
                 <FeedPhoto key={photo.id} photo={photo} />
             ))}
-            <div className="w-full h-8 bg-background" aria-hidden="true"></div>
         </div>
     );
 }
